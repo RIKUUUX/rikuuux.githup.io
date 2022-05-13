3c4458ad88cd55068ea92786d232e2a224af7842
rikuuux <104189461+rikuuux@users.noreply.github.com>
2022-05-13 14:35:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+
+x=0;
+y=0;
+
+dx=2;
+dy=2;
+
+function deplacement() {
+	document.getElementById('balle').style.left=x+dx+'px';
+	x=x+dx;
+	document.getElementById('balle').style.top=y+dy+'px';
+	y=y+dy;
+	console.log(x,y)
+
+}
+
+setInterval(deplacement,10)
+
+
+
+
+
+
+
+
+
+
+
